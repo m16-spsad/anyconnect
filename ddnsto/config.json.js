6834829689722dfa52200ddec130423aa617b857
m16-spsad <67814219+m16-spsad@users.noreply.github.com>
2021-07-23 23:50:15 +0800

    Delete ddnsto directory

--- ddnsto/config.json.js
+++
@@ -1,9 +0,0 @@
-{
-"version":"2.1",
-"md5":"d7cc1f9355f625e9dedbff27974648b7",
-"home_url":"Module_ddnsto.asp",
-"title":"DDNSTO 远程穿透",
-"description":"支持http2协议的远程管理，仅支持远程管理路由器+NAS+Windows远程桌面",
-"changelog":"更新验证方式",
-"build_date":"2021-01-21_23:48:51"
-}
