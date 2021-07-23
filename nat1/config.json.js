2cc259367b28140f81549cc6485bba32cf7f8f55
m16-spsad <67814219+m16-spsad@users.noreply.github.com>
2021-07-23 23:56:05 +0800

    Delete nat1 directory

--- nat1/config.json.js
+++
@@ -1,9 +0,0 @@
-{
-"version":"0.3",
-"md5":"fc51369fd7c0482aeff2e0d7b58f7da7",
-"home_url":"Module_nat1.asp",
-"title":"Full cone NAT",
-"description":"NAT类型-Full cone",
-"changelog":"开启高版本设置",
-"build_date":"2021-02-20_23:24:54"
-}
