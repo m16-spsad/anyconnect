4e6a49a3e9a93707b7826dd7d5ff3466423b1e69
m16-spsad <67814219+m16-spsad@users.noreply.github.com>
2021-07-23 23:53:52 +0800

    Delete unifi directory

--- unifi/config.json.js
+++
@@ -1,9 +0,0 @@
-{
-"version":"1.1",
-"md5":"3ecd0ebc9de94588a79f12417a584806",
-"home_url":"Module_unifi.asp",
-"title":"Unifi Controller",
-"description":"Debian系统和Unifi控制器",
-"changelog":"修复key下载失效",
-"build_date":"2020-05-29_13:21:17"
-}
