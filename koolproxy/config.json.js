9903d3944393dad6f30ffa0823534b2fdf7aa3f8
m16-spsad <67814219+m16-spsad@users.noreply.github.com>
2021-07-24 00:39:36 +0800

    Delete koolproxy directory

--- koolproxy/config.json.js
+++
@@ -1,9 +0,0 @@
-{
-"version":"3.8.6",
-"md5":"ebba79cf973d0a976f1e463e9f80d32d",
-"home_url":"Module_koolproxy.asp",
-"title":"koolproxy",
-"description":"听说KP和软路由更搭哦~",
-"changelog":"修复遗留问题",
-"build_date":"2021-02-17_23:54:40"
-}
