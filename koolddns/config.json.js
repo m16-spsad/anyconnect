6e82978cf521d9dea9256099d75c3ce245db04fc
m16-spsad <67814219+m16-spsad@users.noreply.github.com>
2021-07-23 23:51:29 +0800

    Delete koolddns directory

--- koolddns/config.json.js
+++
@@ -1,9 +0,0 @@
-{
-"version":"0.6.3",
-"md5":"3d5e0815bda5ce0dc0b470c18f0e572f",
-"home_url":"Module_koolddns.asp",
-"title":"Koolddns",
-"description":"动态域名解析工具",
-"changelog":"修复cloudflare API",
-"build_date":"2020-05-22_21:49:11"
-}
